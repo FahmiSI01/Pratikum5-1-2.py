b7fc82b63c32b9acd92606a6ce8b5534f24c5c8f
MuhammadFahmiSI01 <146051240+FahmiSI01@users.noreply.github.com>
2023-10-25 16:14:47 +0700

    Add files via upload

---
+++ Tugas1variabel.py
@@ -0,0 +1,11 @@
+#Tugas1pertemuan5
+
+motor = ["Beat karbu", "metic", "140cc", "orange", "roda dua"]
+print(motor)
+
+motor.append("15.000.000")
+motor.append("sepeda motor")
+print(motor)
+
+motor.insert(2, "honda")
+print(motor)
